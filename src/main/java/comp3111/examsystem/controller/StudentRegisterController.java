4e2a28250617db0d0194d00c7a5c68722e979fef
BryanYuen <bryanyuen5@gmail.com>
2024-11-12 17:47:21 +0800

    Implemented register function for StudentRegisterController

--- src/main/java/comp3111/examsystem/controller/StudentRegisterController.java
+++ src/main/java/comp3111/examsystem/controller/StudentRegisterController.java
@@ -1,5 +1,8 @@
 package comp3111.examsystem.controller;
 
+import comp3111.examsystem.model.Student;
+import comp3111.examsystem.service.Database;
+import comp3111.examsystem.service.MsgSender;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -7,11 +10,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class StudentRegisterController implements Initializable {
+    private Database<Student> studentDatabase;
 
     @FXML
     private TextField ageTxt;
@@ -41,14 +46,42 @@ public class StudentRegisterController implements Initializable {
     private TextField usernameTxt;
 
     @FXML
-    void closeWindow(ActionEvent event) {
-
+    void closeWindow() {
+        Stage stage = (Stage) usernameTxt.getScene().getWindow();
+        stage.close();
     }
 
     @FXML
     void register(ActionEvent event) {
-
+        String age = ageTxt.getText();
+        String department = departmentTxt.getText();
+        String gender = genderChoice.getValue();
+        String name = nameTxt.getText();
+        String username = usernameTxt.getText();
+        String password = passwordTxt.getText();
+        String passConfirm = passwordConfirmTxt.getText();
+        if(validateFields(age, department, gender, name, username, password, passConfirm)){
+            MsgSender.showMsg("Registration Successful! You can now log in.");
+            closeWindow();
+        }
     }
+
+    private boolean validateFields(String age, String department, String gender, String name, String username, String password, String passConfirm){
+        if(age.isEmpty() || department.isEmpty() || gender.isEmpty() || name.isEmpty() || username.isEmpty()){
+            MsgSender.showMsg("All fields should be filled.");
+            return false;
+        }
+        else if (!age.matches("\\d+")) {
+            MsgSender.showMsg("Age must be a number.");
+            return false;
+        }
+        else if(!password.equals(passConfirm)){
+            MsgSender.showMsg("Passwords do not match");
+            return false;
+        }
+        return true;
+    }
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
